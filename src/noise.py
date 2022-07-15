5c17e0fefca8b01d038f7731840d886b87905ab3
thelong-zju <1172110284@qq.com>
2022-07-15 10:26:07 +0800

    2022/7/5/1026

--- src/noise.py
+++ src/noise.py
@@ -1,34 +1,36 @@
 from maya import cmds
-from opensimplex import OpenSimplex
 import random
 import math
 
-cmds.select(all=True)
-cmds.delete()
+def noiseMap(width, height, scale):
+    noise = [[r for r in range(int(width))] for i in range(int(height))]
 
-x=int(input())
+    for i in range(0,int(height)):
+        for j in range(0,int(width)):
+            noise[i][j] = scale * (random.random()*0.8+0.2) # (0,1]
+    return noise
 
-gen = OpenSimplex(x)#要输入数据
-def noise(nx, ny):
-    # Rescale from -1.0:+1.0 to 0.0:1.0
-    #return gen.noise2(nx, ny) / 2.0 + 0.5
-    return gen.noise2(nx,ny)+random.random()*0.5+0.5
+def Elevation(terrain, width, height, sharpness):
+	noiseMap3 = noiseMap(width/4, height/4, 1.4)
+	noiseMap2 = noiseMap(width/2, height/2, 1.2)
+	noiseMap1 = noiseMap(width, height, 1.0)
+	for y in range(height):
+		for x in range(width):
+			pointy = noiseMap3[int(x/4)][int(y/4)] + 0.4* noiseMap2[int(x/2)][int(y/2)] + 0.2* noiseMap1[x][y]
+			pointy = math.pow(pointy, sharpness)
+			cmds.move(0, pointy*0.08, 0, terrain+".vtx["+str(y*width+x)+"]", r=True)
+		cmds.refresh(f = True)
+    
+if __name__ == "__main__":
+	cmds.select(all=True)
+	cmds.delete()
 
-height=33
-width=33
-
-terrain = cmds.polyPlane( axis=[0,1,0], w=10, h=10, sx=32, sy=32, ch=False)
-
-value = []
-for y in range(height):
-    value.append([0] * width)
-    for x in range(width):
-        nx = x/width - 0.5
-        ny = y/height - 0.5
-        value[y][x] = noise(nx, ny)
-
-for i in range(height):
-	for j in range(width):
-		cmds.move(0, value[i][j], 0, terrain[0]+".vtx["+str(i*width+j)+"]", r=True)
-cmds.polySmooth(kb=False)
-print(value)
+	height=200
+	width=200
+	smoothness = 2
+	sharpness = 6.0
+	terrain=cmds.polyPlane( axis=[0,1,0], w=50, h=50, sx=width-1, sy=height-1)
+	Elevation(terrain[0], width, height, sharpness)    
+	cmds.polyAverageVertex(iterations = smoothness)
+	cmds.select(terrain)
+	cmds.polySmooth()
